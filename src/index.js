6b0233d910b7718d76413b3ff2a4c8b3a6406309
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-23 14:27:47 -0400

    Add initDOM'

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles.css';
 import './reset.css';
+import './initDOM.js';
+import initDOM from './initDOM.js';
+// initialize DOM
+initDOM();
---
+++ src/initDOM.js
@@ -0,0 +1,5 @@
+const initDOM = () => {
+   const content = document.getElementById('content');
+   content.textContent = 'Dom Test';
+};
+export default initDOM;
