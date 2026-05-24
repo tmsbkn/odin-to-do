ca9c22e37513f477826742af7795a1de3261fcba
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-24 19:20:14 -0400

    Create Project Class

--- src/class.js
+++ src/class.js
@@ -1,8 +1,15 @@
-class ToDo {
+class Task {
    constructor(title, description, dueDate, priority) {
       this.title = title;
       this.description = description;
       this.dueDate = dueDate;
       this.priority = priority;
+   }
+}
+
+class Project {
+   constructor(title) {
+      this.title = title;
+      this.tasks = [];
    }
 }
