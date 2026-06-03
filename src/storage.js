07c9d32b4483452b6f185e3c402ed3e9d73a36c8
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-06-03 19:04:13 -0400

    add Storage manipulator

---
+++ src/storage.js
@@ -0,0 +1,33 @@
+import Project from './project';
+import Task from './task';
+
+const Storage = (() => {
+   const save = (projects) => {
+      localStorage.setItem('todoApp', JSON.stringify(projects));
+   };
+   const load = () => {
+      const data = localStorage.getItem('todoApp');
+      if (!data) return null;
+
+      const parsed = JSON.parse(data);
+      return parsed.map((projectData) => {
+         const project = new Project(projectData.name);
+         project.id = projectData.id;
+         project.tasks = projectData.tasks.map((taskData) => {
+            const task = new Task(
+               taskData.title,
+               taskData.description,
+               taskData.dueDate,
+               taskData.priority,
+            );
+            task.id = taskData.id;
+            task.done = taskData.done;
+            return task;
+         });
+         return project;
+      });
+   };
+   return { save, load };
+})();
+
+export default Storage;
