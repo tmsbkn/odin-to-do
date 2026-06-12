abbfde76852ac98609cf5e55ca0859a662c39951
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-06-12 14:04:57 -0400

    Tweaks

--- src/storage.js
+++ src/storage.js
@@ -9,23 +9,31 @@ const Storage = (() => {
       const data = localStorage.getItem('todoApp');
       if (!data) return null;
 
-      const parsed = JSON.parse(data);
-      return parsed.map((projectData) => {
-         const project = new Project(projectData.name);
-         project.id = projectData.id;
-         project.tasks = projectData.tasks.map((taskData) => {
-            const task = new Task(
-               taskData.title,
-               taskData.description,
-               taskData.dueDate,
-               taskData.priority,
-            );
-            task.id = taskData.id;
-            task.done = taskData.done;
-            return task;
+      try {
+         const parsed = JSON.parse(data);
+
+         return parsed.map((projectData) => {
+            const project = new Project(projectData.name);
+            project.id = projectData.id;
+
+            project.tasks = projectData.tasks.map((taskData) => {
+               const task = new Task(
+                  taskData.title,
+                  taskData.description,
+                  taskData.dueDate,
+                  taskData.priority,
+               );
+               task.id = taskData.id;
+               task.done = taskData.done;
+               return task;
+            });
+            return project;
          });
-         return project;
-      });
+      } catch (e) {
+         console.warn('Storage Corrupted, resetting:', e);
+         localStorage.removeItem('todoApp');
+         return null;
+      }
    };
    return { save, load };
 })();
